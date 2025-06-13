51f859df3ee431843eabf2906ce3daaada861875
vishalicious213 <vishalicious213@gmail.com>
2025-06-12 17:50:06 -0700

    getting posts & logging them

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("WP Functions")
+const main = document.getElementById("main")
+const url = "https://public-api.wordpress.com/wp/v2/sites/neophyte.home.blog/posts"
+const posts = getPosts()
+
+async function getPosts() {
+    const response = await fetch(url)
+    
+    if (!response.ok) {
+        throw new Error(`HTTP error: ${response.status}`)
+    }
+
+    const data = await response.json()
+    return data
+}
+
+posts
+.then(data => {
+    console.log(data)
+})
+.catch(err => {
+    console.error(`Could not get posts: ${err}`)
+})
