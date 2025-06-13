b284c2770d5600e1f16ef00bea552bd5f3dec76f
vishalicious213 <vishalicious213@gmail.com>
2025-06-13 10:11:28 -0700

    added selectWebsite()

--- index.js
+++ index.js
@@ -1,9 +1,36 @@
 const main = document.getElementById("main")
-const baseUrl = "https://public-api.wordpress.com/wp/v2/sites/neophyte.home.blog"
+const website = document.getElementById("website")
+let baseUrl = "https://public-api.wordpress.com/wp/v2/sites/neophyte.home.blog"
 // const baseUrl = "https://public-api.wordpress.com/wp/v2/sites/uglybass.wordpress.com"
 // const baseUrl = "https://public-api.wordpress.com/wp/v2/sites/bt4wall.wordpress.com"
 // const baseUrl = "https://public-api.wordpress.com/wp/v2/sites/mortalequality.wordpress.com"
 const posts = getPosts()
+
+// ⬇️ EVENT LISTENERS ⬇️
+
+website.addEventListener("click", selectWebsite)
+
+// ⬇️ EVENT HANDLERS ⬇️
+
+function selectWebsite() {
+    console.log(website.value)
+
+    if (website.value === "code") {
+        baseUrl = "https://public-api.wordpress.com/wp/v2/sites/neophyte.home.blog"
+    }
+
+    if (website.value === "bass") {
+        baseUrl = "https://public-api.wordpress.com/wp/v2/sites/uglybass.wordpress.com"
+    }
+
+    if (website.value === "hobby") {
+        baseUrl = "https://public-api.wordpress.com/wp/v2/sites/bt4wall.wordpress.com"
+    }
+
+    if (website.value === "music") {
+        baseUrl = "https://public-api.wordpress.com/wp/v2/sites/mortalequality.wordpress.com"
+    }
+}
 
 // get posts
 async function getPosts() {
@@ -18,10 +45,10 @@ async function getPosts() {
 }
 
 // log posts to console
-posts
-.then(data => {
-    console.log(data)
-})
-.catch(err => {
-    console.error(`Could not get posts: ${err}`)
-})
+// posts
+// .then(data => {
+//     console.log(data)
+// })
+// .catch(err => {
+//     console.error(`Could not get posts: ${err}`)
+// })
