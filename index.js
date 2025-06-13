4aa49e1c52d105651e3b1aa78a3c37c9f767c817
vishalicious213 <vishalicious213@gmail.com>
2025-06-13 09:57:01 -0700

    tested baseUrl with other blogs & they all worked

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const main = document.getElementById("main")
 const baseUrl = "https://public-api.wordpress.com/wp/v2/sites/neophyte.home.blog"
+// const baseUrl = "https://public-api.wordpress.com/wp/v2/sites/uglybass.wordpress.com"
+// const baseUrl = "https://public-api.wordpress.com/wp/v2/sites/bt4wall.wordpress.com"
+// const baseUrl = "https://public-api.wordpress.com/wp/v2/sites/mortalequality.wordpress.com"
 const posts = getPosts()
 
+// get posts
 async function getPosts() {
     const response = await fetch(`${baseUrl}/posts`)
     
@@ -13,6 +17,7 @@ async function getPosts() {
     return data
 }
 
+// log posts to console
 posts
 .then(data => {
     console.log(data)
